cdb45907befa3632900d2cd0da54fc67a2aaf733
partorican <ibringwater@gmail.com>
2019-12-13 03:58:59 -0700

    a lot done on modal

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import App from './App.jsx';
+const faker = require('faker');
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -19,12 +21,23 @@ function getModalStyle() {
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    position: 'absolute',
-    width: 400,
+    position: 'relative',
+    display: 'flex',
+    minWidth: 0,
+    minHeight: 0,
+    height: 600,
+    maxWidth: 600,
+    maxHeight: 600,
+    // margin: '2%',
+    right: '1%',
+    left: '1%',
+    top: '20%',
+    //bottom: '0',
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    //border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+    outline: 0,
   },
 }));
 
@@ -51,23 +64,112 @@ export default function SimpleModal() {
 
   return (
     <div>
-      <button className="modalCart" type="button" onClick={handleOpen} style={buttonStyle}>
+
+      <button className="modalCart"
+        type="button"
+        onClick={handleOpen}
+        style={buttonStyle}>
         🛒
       </button>
-      <Modal
-        aria-labelledby="simple-modal-title"
+
+      <Modal aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
         open={open}
-        onClose={handleClose}
-      >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Text in a modal</h2>
-          <p id="simple-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
+        onClose={handleClose}>
+
+        <div className={classes.paper} style={modalStyle}>
+
+          <table>
+
+            <tbody>
+              <tr>
+                {/* table 1 - 1 row 3 columns */}
+                <td>
+                  <div className="modalHeader">Added to Cart</div>
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  <div className="modalHeader">Cart Summary</div>
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              {/* table 2 - 1 row 3 columns */}
+              <tr>
+                <td>
+                  <div className="modalImage">
+                    <img id="simple-modal-title" src={`${faker.image.image()}`} />
+                  </div>
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  <p id="modalQty" className="currentItem"> ____ item(s): $777.77</p>
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              {/* table 3 - 3 row 2 columns */}
+              <tr>
+                <td>
+                  <p className="currentItem" id="simple-modal-description">Item in 🛒: <span className="itemName">{ faker.commerce.productName() } </span></p>
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+              </tr>
+              <tr>
+                <td className="currentItem">
+                  Stainless Steel
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p id="modalQty" className="currentItem"> Quantity __</p>
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              {/* table 4 - 1 row 2 columns */}
+              <tr>
+                <td>
+                  <button className="keepShopping" onClick={handleClose}>Keep Shopping</button>
+                </td>
+                <td>
+                  {/* blank */}
+                </td>
+                <td>
+                  <button className="checkout" onClick={handleClose}>View Cart & Checkout</button>
+                </td>
+              </tr>
+            </tbody>
+
+          </table>
 
         </div>
       </Modal>
+
     </div>
   );
 }
